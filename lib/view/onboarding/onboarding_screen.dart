47bbec4daee1c233d035ad0df0b5153b3f791d87
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-01-18 21:58:34 +0545

    Feat(On Boarding): On Boarding Screen and its Functionality Added

---
+++ lib/view/onboarding/onboarding_screen.dart
@@ -0,0 +1,207 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app_ui/constant/color.dart';
+import 'package:travel_app_ui/model/on_boarding_model.dart';
+import 'package:travel_app_ui/view/home/home_screen.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView.builder(
+            // pageSnapping: false,
+            itemCount: onboarding.length,
+            onPageChanged: (value) {
+              setState(() {
+                currentIndex = value;
+              });
+            },
+            itemBuilder: (context, index) {
+              return Image.network(onboarding[index].image, fit: BoxFit.cover);
+            },
+          ),
+
+          Padding(
+            padding: EdgeInsetsGeometry.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 40),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomeScreen()),
+                    );
+                  },
+                  child: Visibility(
+                    visible: currentIndex == onboarding.length - 1
+                        ? false
+                        : true,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 15,
+                            vertical: 7,
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(color: Colors.white54, width: 2),
+                          ),
+                          child: Text(
+                            "Skip",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Text(
+                  onboarding[currentIndex].name,
+                  style: TextStyle(
+                    fontSize: 70,
+                    color: Colors.white,
+                    height: 1,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "We Travelin are ready to help you on\nvacation around Nepal",
+                  style: TextStyle(
+                    fontSize: 17,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Positioned(
+            bottom: 0,
+            right: 0,
+            left: 0,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List.generate(onboarding.length, (index) {
+                    return AnimatedContainer(
+                      margin: EdgeInsets.only(right: 15),
+                      height: 6,
+                      width: 30,
+                      decoration: BoxDecoration(
+                        color: currentIndex == index
+                            ? Colors.white
+                            : Colors.grey,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      duration: Duration(milliseconds: 400),
+                    );
+                  }),
+                ),
+                SizedBox(height: 15),
+            
+                Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => HomeScreen(),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          height: 70,
+                          decoration: BoxDecoration(
+                            color: kButtonColor,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Let's Get Started",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              SizedBox(width: 10),
+                              Icon(Icons.arrow_forward, color: Colors.white),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(
+                              builder: (context) => HomeScreen(),
+                            ),
+                          );
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Already have an account?",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Text(
+                              " Login",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w800,
+                                color: blueTextColor,
+                                fontSize: 17,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
